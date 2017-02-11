2324dd374e973fb0e670c6bfa07231270f2a96f7
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-11 11:01:06 -0600

    handle posix events

--- 04-local-system/process.js
+++ 04-local-system/process.js
@@ -4,4 +4,7 @@ stdin.resume();
 stdin.setEncoding('utf8');
 
 stdin.on('data', chunk => stdout.write(`:: ${chunk}`));
-stdin.on('end', chunk => stderr.write(`:: DONE!`));
+stdin.on('end', () => stderr.write(':: DONE!'));
+stdin.on('SIGTERM', () => stderr.write(':: TERMINATING!'));
+
+console.log(`Running process #${process.pid}`);
