6ccefabac78678d3bdaf6d5c11fa529a3594b71c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-11 10:40:41 -0600

    piping readable stream to writable stream through a read-write stream

--- 03-events-and-streams/pipe.js
+++ 03-events-and-streams/pipe.js
@@ -2,11 +2,47 @@ const fs = require('fs');
 const request = require('request');
 
 const dir = './out';
-if (!fs.existsSync(dir)){
+if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
 
 const source = request('http://www.google.com');
-const destination = fs.createWriteStream(`${dir}/google.html`);
 
-source.pipe(destination);
+(() => {
+
+  /**
+   * from read stream (request) to write stream (stdout)
+   */
+
+  const destination = process.stdout;
+
+  source.pipe(destination);
+
+})();
+
+(() => {
+
+  /**
+   * from read stream (request) to write stream (file)
+   */
+
+  const destination = fs.createWriteStream(`${dir}/google.html`);
+
+  source.pipe(destination);
+
+})();
+
+(() => {
+
+  /**
+   * from read stream (request) to read-write stream (zlib) to write stream (file)
+   */
+
+  const zlib = require('zlib');
+
+  const gzipper = zlib.createGzip();
+  const destination = fs.createWriteStream(`${dir}/google.html.gz`);
+
+  source.pipe(gzipper).pipe(destination);
+
+})();
